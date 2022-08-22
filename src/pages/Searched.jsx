325c427e4637f324d6618974c046dd3956b8ec05
Kaung Myat Tun <kaungtun111@gmail.com>
2022-08-22 22:16:57 +0630

    adding animation with framer-motion

--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import styled from 'styled-components'
 
 const Searched = () => {
@@ -22,8 +22,10 @@ const Searched = () => {
         {searchedRecipes.map((item) => {
           return (
               <Card key={item.id}>
-                  <img src={item.image} alt="" />
-                  <h4>{item.title}</h4>
+                  <Link to={'/recipe/' + item.id}>
+                      <img src={item.image} alt="" />
+                      <h4>{item.title}</h4>
+                  </Link>
               </Card>
           )
         })}
